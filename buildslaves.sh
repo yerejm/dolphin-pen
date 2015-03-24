4bbfecb0d37353cfb99be226d9f9d9b47d1cd0ad
yerejm <github@thelostyerejm.com>
2015-03-24 21:09:55 +1000

    rename buildslave playbook and provide wrapper to call it

---
+++ buildslaves.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+set -o pipefail
+ansible buildslaves -m ping -i ansible/inventory -u root
+rc=$?
+if [ "$rc" != "0" ]; then
+    echo "Servers not ready..."
+    exit $rc
+fi
+ansible-playbook ansible/buildslaves.yml -i ansible/inventory -l buildslaves
+

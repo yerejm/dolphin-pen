7b8b37a7f98ad7789739f2c13c0ca502f25798cb
yerejm <github@thelostyerejm.com>
2015-05-23 20:18:55 +1000

    Wait for servers to be ready after creation before provisioning.

--- buildslaves.sh
+++ buildslaves.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -eu
 set -o pipefail
-ansible-playbook ansible/ping.yml -i ansible/inventory
-rc=$?
-if [ "$rc" != "0" ]; then
-    echo "Servers not ready..."
-    exit $rc
-fi
 ansible-playbook ansible/buildslaves.yml -i ansible/inventory -e 'testing=yes'
 
